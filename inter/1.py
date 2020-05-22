b0c701762856541016f62ab1399a6a1a862bc145
haoxiaomin <2645627070@qq.com>
2020-05-22 16:47:59 +0800

    6-10

--- inter/1.py
+++ inter/1.py
@@ -152,6 +152,7 @@
 # c = True
 # print (type(a),type(b),type(c)) # <type 'list'> <type 'dict'> <type 'bool'>
 # print (isinstance(a,list))  # True
+
 # 6 字典推导式
 # 可能你见过列表推导时,却没有见过字典推导式,在2.7中才加入的:
 # d = {key: value for (key, value) in iterable}
